e16caea879dc1609d6f78e4fb5a618d343fa2d54
anetelusina <anetelusina@gmail.com>
2023-04-02 22:47:19 +0100

    Added design (function) for active and inactive temperature links

--- src/index.js
+++ src/index.js
@@ -101,12 +101,16 @@ function showGeolocation(position) {
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
